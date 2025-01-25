e21b7e9d260023b46ca55b277a7c522fe944a006
Delwin <justinkuhn@t-online.de>
2025-01-25 21:51:57 +0100

    add turret selection ui logic

--- scenes/ui/turret_selection.gd
+++ scenes/ui/turret_selection.gd
@@ -2,15 +2,27 @@ extends Control
 
 @onready var container : VBoxContainer = $VBoxContainer
 @export_range(0, 10, 1.0) var initial_turret: int = 0
+@onready var selected_turret : int = initial_turret;
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	update_selected_turret()
+
+func _input(event: InputEvent) -> void:
+	if (event.is_action_pressed("turret_selection_down")):
+		selected_turret += 1
+		if (selected_turret + 1 > container.get_child_count()):
+			selected_turret = 0
+		update_selected_turret()
+			
+	if (event.is_action_pressed("turret_selection_up")):
+		selected_turret -= 1
+		if (selected_turret < 0):
+			selected_turret = container.get_child_count() - 1
+		update_selected_turret()
+		
+
+func update_selected_turret() -> void:
 	var i = 0;
 	for turret_option in container.get_children():
-		if (i == initial_turret):
-			turret_option.selected = true
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+		turret_option.selected = i == selected_turret
+		i += 1
